40a614ba54d307b6cf0a0fff43af9ca954384509
pol <plabaye@etu.unistra.fr>
2022-09-29 21:53:16 +0200

    modified json names for entry

--- adjustment_gen.py
+++ adjustment_gen.py
@@ -5,11 +5,11 @@ url = 'http://127.0.0.1:8000/adjustment'
 headers = {'Content-Type': 'application/json'}
 
 data = {
-    "warehouseID":"MAG1",
-    "locationID":"A-02-02-03",
-    "itemID":"E961307",
-    "itemQuantity":"98",
-    "loginCode":"aia",
+    "wid":"MAG1",
+    "location":"A-02-02-03",
+    "pid":"E961307",
+    "quantity":"98",
+    "login":"aia",
 }
 
 d = json.dumps(data)
